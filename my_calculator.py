fa725766818ebc5f0233243b6f270a485e932262
Sumayia Rashid <71390091+sumayiar@users.noreply.github.com>
2025-10-08 11:53:47 -0400

    Update my_calculator.py

--- my_calculator.py
+++ my_calculator.py
@@ -1,28 +1,40 @@
+#This is a simple script to simulate a simple calculator based on user input.
+
+#Build four functions to represent the four basic operations:
+
+#Addition
 def add(x, y):
     return x + y
 
+#Subtraction
 def subtract(x, y):
     return x - y
 
+#Multiplication
 def multiply(x, y):
     return x * y
 
+#Division
 def divide(x, y):
     if y == 0:
         return "Cannot divide by zero!"
     return x / y
 
+#Get user input
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 
+#Gather which operation the user wants to use.
 choice = input("Enter choice (1/2/3/4): ")
 
+#Gather the numbers the user wants to operate with.
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
+#Based on user input, operate.
 if choice == '1':
     print(f"Result: {add(num1, num2)}")
 elif choice == '2':
